c991e3676a476b9b50f7e03922254071ae76a7f9
LeHuyTu <101401285+Jay-Huy@users.noreply.github.com>
2025-04-17 21:33:07 +0700

    Update utils.py

--- utils.py
+++ utils.py
@@ -148,7 +148,7 @@ def step(model, tokenizer, data_loader, optimizer, criterion, device, cer, train
         
     for i, batch in tqdm(enumerate(data_loader)):
         # Move batch to device
-        if i == 50: break # For testing purposes, remove this line in production
+        # if i == 50: break # For testing purposes, remove this line in production
         batch = {key: value.to(device) if isinstance(value, torch.Tensor) else value for key, value in batch.items()}
 
         # Example: 而 对 楼市 成交 抑制 作用 最 大 的 限 购</s>
